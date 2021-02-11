f4343e7b43718a5a1617956efa5c95f42ca91247
Puru80 <puru.agar99@gmail.com>
2021-02-11 20:30:07 +0530

    Commit 11 February 2021

--- src/main/java/com/example/springmongo/repositories/PetsRepository.java
+++ src/main/java/com/example/springmongo/repositories/PetsRepository.java
@@ -4,7 +4,9 @@ import com.example.springmongo.models.Pets;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetsRepository extends MongoRepository<Pets, String>
 {
     Pets findBy_id(ObjectId id);
